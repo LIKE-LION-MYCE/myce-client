73d56e139ffe1db771e2c6e9a6566e36f103bf45
jihyun <qlfus010@naver.com>
2025-08-01 15:01:52 +0900

    feat : 플랫폼 관리자 문의 페이지

--- src/routes/PlatformAdminRoutes.jsx
+++ src/routes/PlatformAdminRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import PlatformAdminLayout from '../platform-admin/layout/PlatformAdminLayout';
 import PlatformDashboard from '../platform-admin/pages/platformDashboard/platformDashboard';
 import SettlementHistory from '../platform-admin/pages/settlementHistory/SettlementHistory';
+import PlatformInquiry from '../platform-admin/pages/platformInquiry/PlatformInquiry';
 
 function PlatformAdminRoutes() {
   return (
@@ -9,6 +10,7 @@ function PlatformAdminRoutes() {
         <Route path="/platform/admin" element={<PlatformAdminLayout />}>
             <Route index element={<PlatformDashboard/>}/>
             <Route path="settlementHistory" element={<SettlementHistory/>}/>
+            <Route path="inquiry" element={<PlatformInquiry/>}/>
         </Route>
       </Routes>
   );
