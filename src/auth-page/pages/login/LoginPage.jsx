d735c3953fb17de788d8b8e03dd8ad2e0df24d61
Lee Chae Rin <chaerin0916@gmail.com>
2025-08-02 19:21:43 +0900

    feat: 비밀번호 찾기

--- src/auth-page/pages/login/LoginPage.jsx
+++ src/auth-page/pages/login/LoginPage.jsx
@@ -110,9 +110,9 @@ const LoginPage = () => {
           </div>
 
           <div className={styles.loginFooter}>
-            <a href="#">아이디 찾기</a>
+            <a href="/findId">아이디 찾기</a>
             <span>|</span>
-            <a href="/findId">비밀번호 찾기</a>
+            <a href="/findPassword">비밀번호 찾기</a>
             <span>|</span>
             <a href="/signup">회원가입</a>
           </div>
--- src/routes/AuthPageRoutes.jsx
+++ src/routes/AuthPageRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../auth-page/pages/login/LoginPage";
 import SignUpPage from "../auth-page/pages/signup/SignUpPage";
 import FindIdPage from "../auth-page/pages/findId/FindIdPage";
+import FindPasswordPage from "../auth-page/pages/findPassword/FindPasswordPage";
 
 const AuthRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AuthRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/findId" element={<FindIdPage />} />
+      <Route path="/findPassword" element={<FindPasswordPage />} />
     </Routes>
   );
 };
