d27ffd6a60650cc285ab9f70ff58fe0a479bd78a
Jihyun <qlfus010@naver.com>
2025-08-03 02:44:21 +0900

    feat : 현재 박람회 관리

--- src/routes/PlatformAdminRoutes.jsx
+++ src/routes/PlatformAdminRoutes.jsx
@@ -7,6 +7,8 @@ import RoleAdmins from '../platform-admin/pages/roleAdmins/RoleAdmins';
 import RoleUsers from '../platform-admin/pages/roleUsers/RoleUsers';
 import ExpoApplications from '../platform-admin/pages/expoApplications/ExpoApplications';
 import ExpoApplicationDetail from '../platform-admin/pages/expoApplicationsDetail/ExpoApplicationDetail';
+import ExpoCurrent from '../platform-admin/pages/expoCurrent/ExpoCurrent';
+import ExpoCurrentDetail from '../platform-admin/pages/expoCurrentDetail/ExpoCurrentDetail';
 
 function PlatformAdminRoutes() {
   return (
@@ -19,6 +21,8 @@ function PlatformAdminRoutes() {
             <Route path="roleUsers" element={<RoleUsers/>}/>
             <Route path="expoApplications" element={<ExpoApplications/>}/>
             <Route path="expoApplications/:id" element={<ExpoApplicationDetail />} />
+            <Route path="expoCurrent" element={<ExpoCurrent />} />
+            <Route path="expoCurrent/:id" element={<ExpoCurrentDetail />} />
         </Route>
       </Routes>
   );s
