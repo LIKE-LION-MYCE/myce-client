437155804fdb3f0777d376110f4d6d92205c1a5a
ufo9430 <ufo9430@naver.com>
2025-08-04 00:20:35 +0900

    feat: axios 및 vite 프록시 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // 프론트 포트
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 스프링 서버
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
