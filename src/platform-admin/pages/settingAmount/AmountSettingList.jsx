e165074979c69a61647b519730d1ca1ade9a6f5f
Jihyun <qlfus010@naver.com>
2025-08-04 01:53:09 +0900

    refactor : 금액 설정 메인 페이지 스타일 변경
    금액 설정 메인 페이지 스타일 변경

--- src/platform-admin/pages/settingAmount/AmountSettingList.jsx
+++ src/platform-admin/pages/settingAmount/AmountSettingList.jsx
@@ -1,29 +1,24 @@
-// src/pages/AmountSettingPage.jsx
-import React from 'react';
+// src/pages/AmountSettingList.jsx
 import { Link } from 'react-router-dom';
 import styles from './AmountSettingList.module.css';
 
 const cardData = [
   {
-    color: 'purple',
     title: '광고 이용료',
     name: 'advertisingFee',
     desc: '광고 서비스 이용에 대한 수수료를 설정합니다',
   },
   {
-    color: 'green',
     title: '박람회 등록비',
     name: 'expoRegistrationFee',
     desc: '박람회 등록비 정산 금액을 설정합니다',
   },
   {
-    color: 'red',
     title: '박람회 환불 수수료',
     name: 'expoRefundFee',
     desc: '박람회 참가비 환불 시 적용되는 수수료입니다',
   },
   {
-    color: 'orange',
     title: '사용자 환불 수수료',
     name: 'userRefundFee',
     desc: '일반 사용자 환불 시 적용되는 수수료입니다',
@@ -31,10 +26,10 @@ const cardData = [
 ];
 
 const historyData = [
-  { label: '광고 이용료 변경', color: 'purple', date: '2024.01.15 14:30' },
-  { label: '박람회 환불 수수료 변경', color: 'red', date: '2024.01.15 14:30' },
-  { label: '박람회 등록비 변경', color: 'green', date: '2024.01.15 14:30' },
-  { label: '사용자 환불 수수료 변경', color: 'orange', date: '2024.01.15 14:30' },
+  { label: '광고 이용료 변경', date: '2024.01.15 14:30' },
+  { label: '박람회 환불 수수료 변경', date: '2024.01.15 14:30' },
+  { label: '박람회 등록비 변경', date: '2024.01.15 14:30' },
+  { label: '사용자 환불 수수료 변경', date: '2024.01.15 14:30' },
 ];
 
 export default function AmountSettingList() {
@@ -42,11 +37,8 @@ export default function AmountSettingList() {
     <main className={styles.container}>
       <section className={styles.gridBox}>
         {cardData.map((card) => (
-          <Link to={`/platform/admin/settingAmount/${card.name}`}>
-            <div
-              key={card.title}
-              className={`${styles.card} ${styles[card.color]}`}
-            >
+          <Link to={`/platform/admin/settingAmount/${card.name}`} key={card.name}>
+            <div className={styles.card}>
               <div>
                 <h2 className={styles.cardTitle}>{card.title}</h2>
                 <p className={styles.cardDesc}>{card.desc}</p>
@@ -63,10 +55,7 @@ export default function AmountSettingList() {
           <tbody>
             {historyData.map((item, index) => (
               <tr key={index}>
-                <td>
-                  <span className={`${styles.dot} ${styles[item.color]}`} />
-                  {item.label}
-                </td>
+                <td>{item.label}</td>
                 <td className={styles.date}>{item.date}</td>
               </tr>
             ))}
