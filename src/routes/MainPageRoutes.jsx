1c46874f375e7ea478b68ac62119daefb8b4298c
Lee Chae Rin <chaerin0916@gmail.com>
2025-08-03 15:55:39 +0900

    feat: 비회원 예매 내역 확인

--- src/routes/MainPageRoutes.jsx
+++ src/routes/MainPageRoutes.jsx
@@ -1,19 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import ExpoApply from '../mainpage/pages/expoApply/ExpoApply';
-import ExpoApply2 from '../mainpage/pages/expoApply/ExpoApply2';
-import MainPageLayout from '../mainpage/layout/MainPageLayout';
-import AdApply from '../mainpage/pages/adApply/AdApply';
-
+import { Routes, Route } from "react-router-dom";
+import ExpoApply from "../mainpage/pages/expoApply/ExpoApply";
+import ExpoApply2 from "../mainpage/pages/expoApply/ExpoApply2";
+import MainPageLayout from "../mainpage/layout/MainPageLayout";
+import AdApply from "../mainpage/pages/adApply/AdApply";
+import NonMemberReservationCheckPage from "../mainpage/pages/nonMemberReservation/NonMemberReservationCheckPage";
+import ReservationDetailPage from "../mypage/components/reservationDetail/ReservationDetailPage";
 
 function MainPageRoutes() {
   return (
-      <Routes>
-        <Route path="" element={<MainPageLayout/>}>
-            <Route path="expo-apply" element={<ExpoApply />} />
-            <Route path="expo-apply2" element={<ExpoApply2 />} />
-            <Route path="ad-apply" element={<AdApply />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="" element={<MainPageLayout />}>
+        <Route path="expo-apply" element={<ExpoApply />} />
+        <Route path="expo-apply2" element={<ExpoApply2 />} />
+        <Route path="ad-apply" element={<AdApply />} />
+        <Route path="non-member" element={<NonMemberReservationCheckPage />} />
+        <Route
+          path="/non-member/reservation/:id"
+          element={<ReservationDetailPage />}
+        />
+      </Route>
+    </Routes>
   );
 }
 
