da90e584c4a049eff63dbc7405c0216ec42315f2
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2025-08-03 23:48:47 +0900

    feat: 박람회 참여자 회원정보 확인 구현

--- src/routes/MyPageRoutes.jsx
+++ src/routes/MyPageRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MyPageLayout from "../mypage/layout/MyPageLayout";
 import MyInfoPage from "../mypage/pages/info/MyInfoPage";
+import MyExpoInfoPage from "../mypage/pages/info/MyExpoInfoPage";
 import MyPaymentPage from "../mypage/pages/payment/MyPaymentPage";
 import MyReservationPage from "../mypage/pages/reservation/MyReservationPage";
 import MySavedExpoPage from "../mypage/pages/saved-expo/MySavedExpoPage";
@@ -17,12 +18,12 @@ const MyPageRoutes = () => {
         {/* 기본 진입 시 myInfo로 리다이렉트 */}
         <Route index element={<Navigate to="info" replace />} />
         <Route path="info" element={<MyInfoPage />} />
+        <Route path="info-expo" element={<MyExpoInfoPage />} />
         <Route path="payment" element={<MyPaymentPage />} />
         <Route path="reservation" element={<MyReservationPage />} />
         <Route path="reservation/:id" element={<ReservationDetailPage />} />
         <Route path="saved-expo" element={<MySavedExpoPage />} />
         <Route path="setting" element={<MySettingPage />} />
-
         <Route path="ads-status" element={<AdsStatusPage />} />
         <Route path="ads-status/:id" element={<AdsStatusDetail />} />
         <Route path="expo-status" element={<ExpoStatusPage />} />
