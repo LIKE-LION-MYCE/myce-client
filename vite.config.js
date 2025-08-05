7368de109720f77559fe29aaf86f7dd7e9e061bf
ufo9430 <ufo9430@naver.com>
2025-08-05 11:15:04 +0900

    feat: vite.config.js 빌드 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    port: 5173, // 프론트 포트
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 스프링 서버
+        target: baseURL,
         changeOrigin: true,
       },
     },
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
