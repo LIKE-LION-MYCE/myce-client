62bfa886854f69f18261304820966eb38736528b
ufo9430 <ufo9430@naver.com>
2025-08-05 13:13:45 +0900

    env: 로컬 설정으로 vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: baseURL,
-        changeOrigin: true,
-      },
+export default defineConfig(({ mode }) => {
+  // .env 파일 로드
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
     },
-  },
-  server: {
-    historyApiFallback: true,
-  },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL, // 여기서 env 사용
+          changeOrigin: true,
+        },
+      },
+    // server: {
+    //   historyApiFallback: true,
+    // },
+    },
+  };
 });
