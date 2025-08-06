7ad3af4623e84c7d469c8281860a0d52d5f13165
choeihwa <chldlghk@gmail.com>
2025-08-06 14:43:09 +0900

    feat: 로그인 API 연동 개발

--- src/api/service/auth/AuthService.jsx
+++ src/api/service/auth/AuthService.jsx
@@ -5,4 +5,16 @@ const signup = async ({name, loginId, email, password, birth, phone, gender}) =>
         {name, loginId, email, password, birth, phone, gender});
 }
 
+const login = async(loginId, password) => {
+    console.log('try login!!!! ' + loginId + ' ' + password);
+    return await instance.post(`/auth/login`, {loginId, password}).then(res => {
+        console.log(res);
+        const accessToken = res.headers.authorization;
+        console.log(`Receive accessToken: ${accessToken}`);
+
+        localStorage.setItem('access_token', accessToken.split(" ")[1]);
+        return res;
+    });
+}
+
 export {signup, login};
