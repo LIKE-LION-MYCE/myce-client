2f576b94e0fee590d6c4c59e11c3201f0f9938fd
Jihyun <qlfus010@naver.com>
2025-08-11 03:40:16 +0900

    feat : 예약자 내역 티켓 이름 필터링 연동

--- src/api/service/expo-admin/reservation/ReservationService.jsx
+++ src/api/service/expo-admin/reservation/ReservationService.jsx
@@ -19,3 +19,13 @@ export const getMyExpoReservation = async (expoId, page, size, entranceStatus, n
     throw new Error(message);
   }
 };
+
+export const getExpoTicketNames = async (expoId) => {
+  try {
+    const response = await instance.get(`/expos/${expoId}/reservations/ticket-name`);
+    return response.data;
+  } catch (error) {
+    const message = error.response?.data?.message || "티켓 정보 조회 중 오류 발생";
+    throw new Error(message);
+  }
+};
