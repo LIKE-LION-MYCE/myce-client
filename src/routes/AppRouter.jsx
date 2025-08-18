cf279a6376663746c599d359ef783f9ab4092c74
Jihyun <qlfus010@naver.com>
2025-08-18 11:33:43 +0900

    refactor : 페이지 이동 시 스크롤 맨 위로 초기화

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouteBody from "./AppRouteBody";
+import ScrollToTop from "../common/ScrollToTop";
 
 function AppRouter() {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
       <AppRouteBody/>
     </BrowserRouter>
   );
