20e305510f029cf321194fba6d6be0745394f6ae
ufo9430 <ufo9430@naver.com>
2025-08-18 18:14:45 +0900

    feat: 플랫폼 관리자 권한 접근 제한 기능 구현

--- src/platform-admin/layout/PlatformAdminLayout.jsx
+++ src/platform-admin/layout/PlatformAdminLayout.jsx
@@ -1,26 +1,59 @@
-import { Outlet } from "react-router-dom";
-import styles from './PlatformAdminLayout.module.css'
-import PlatformAdminHeader from "./header/PlatformAdminHeader";
-import PlatformAdminSideBar from "./sidebar/PlatformAdminSideBar";
+  import { Outlet } from "react-router-dom";
+  import { useEffect, useState } from "react";
+  import styles from './PlatformAdminLayout.module.css'
+  import PlatformAdminHeader from "./header/PlatformAdminHeader";
+  import PlatformAdminSideBar from "./sidebar/PlatformAdminSideBar";
+  import AccessDenied from "../../common/pages/AccessDeniedPage"
+  import { jwtDecode } from "jwt-decode";
 
-function PlatformAdminLayout() {
-  return (
-    <div className={styles.layout}>
-      <div className={styles.contentWrapper}>
-        <div className={styles.sidebar}>
-          <PlatformAdminSideBar></PlatformAdminSideBar>
+  function PlatformAdminLayout() {
+    const [hasExpoAccess, setHasExpoAccess] = useState(null);
+
+    useEffect (() => {
+      const checkPermission = async () => {
+        try {
+          const token = localStorage.getItem('access_token');
+          if (!token) {
+            setHasExpoAccess(false);
+          }
+
+          const decodedToken = jwtDecode(token);
+
+          if (decodedToken.role === 'PLATFORM_ADMIN') {
+            setHasExpoAccess(true);
+          }
+        }catch(error){
+          console.log(error);
+        }
+      }
+
+      checkPermission();
+    }, [])
+    
+    if(hasExpoAccess == null){
+      return (
+        <div>
+          <AccessDenied />
         </div>
-        <div className={styles.main}>
-          <div className={styles.header}>
-            <PlatformAdminHeader></PlatformAdminHeader>
+      )
+    }
+    return (
+      <div className={styles.layout}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.sidebar}>
+            <PlatformAdminSideBar></PlatformAdminSideBar>
           </div>
-          <div className={styles.content}>
-            <Outlet />
+          <div className={styles.main}>
+            <div className={styles.header}>
+              <PlatformAdminHeader></PlatformAdminHeader>
+            </div>
+            <div className={styles.content}>
+              <Outlet />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default PlatformAdminLayout;
+  export default PlatformAdminLayout;
