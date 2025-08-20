f7d87709af5814d5f3ecb2f1d3a1a5575ee29b4b
Jihyun <qlfus010@naver.com>
2025-08-20 21:19:45 +0900

    style : 박람회 관리자 UI 수정

--- src/common/components/toastSuccess/ToastSuccess.jsx
+++ src/common/components/toastSuccess/ToastSuccess.jsx
@@ -1,11 +1,11 @@
 import styles from './ToastSuccess.module.css';
 import { FaCheckCircle } from 'react-icons/fa';
 
-function ToastSuccess({message}) {
+function ToastSuccess({ message }) {
   return (
     <div className={styles.toastBox}>
       <FaCheckCircle />
-      {!message ? <span>{message}</span> : <span>성공적으로 처리되었습니다.</span>}
+      <span>{message || '성공적으로 처리되었습니다.'}</span>
     </div>
   );
 }
